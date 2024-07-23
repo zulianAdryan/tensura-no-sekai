564e73d850574106193e747e3fa0e4fc5ba71b6c
zulianAdryan <zulian.adryan@gmail.com>
2024-07-23 14:05:18 +0700

    style: add fonts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -3,6 +3,9 @@ import { twMerge } from "tailwind-merge";
 import {
   Inter as FontSans,
   Noto_Sans_JP as FontNotoSansJapanese,
+  Cherry_Bomb_One as FontCherry,
+  Coiny as FontCoiny,
+  Mochiy_Pop_One as FontMochiy,
 } from "next/font/google";
 
 const fontSans = FontSans({ subsets: ["latin"], variable: "--font-sans" });
@@ -10,9 +13,30 @@ const fontNotoSansJapanese = FontNotoSansJapanese({
   subsets: ["latin"],
   variable: "--font-notojp",
 });
+const fontCherry = FontCherry({
+  subsets: ["latin"],
+  variable: "--font-cherry",
+  weight: "400",
+});
+const fontCoiny = FontCoiny({
+  subsets: ["latin"],
+  variable: "--font-coiny",
+  weight: "400",
+});
+const fontMochiy = FontMochiy({
+  subsets: ["latin"],
+  variable: "--font-mochiy",
+  weight: "400",
+});
 
 export const cn = (...inputs: ClassValue[]) => {
   return twMerge(clsx(inputs));
 };
 
-export const fonts = [fontSans.variable, fontNotoSansJapanese.variable];
+export const fonts = [
+  fontSans.variable,
+  fontNotoSansJapanese.variable,
+  fontCherry.variable,
+  fontCoiny.variable,
+  fontMochiy.variable,
+];
