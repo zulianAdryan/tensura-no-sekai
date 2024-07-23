688d88e9ca92085aa6c814948e3f084e2032f8ca
zulianAdryan <zulian.adryan@gmail.com>
2024-07-23 14:05:03 +0700

    feat: wrapper revealer component

---
+++ src/components/WrapperRevealer.tsx
@@ -0,0 +1,32 @@
+import { useInView } from "framer-motion";
+import { ReactElement, useRef } from "react";
+import { motion } from "framer-motion";
+import { ClassValue } from "clsx";
+import { cn } from "@/lib/utils";
+
+const WrapperRevealer = ({
+  className,
+  children,
+}: {
+  className?: ClassValue;
+  children: React.ReactNode;
+}) => {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+
+  //   console.log("isInView", isInView);
+
+  return (
+    <motion.section
+      className={cn(className)}
+      ref={ref}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: isInView ? 1 : 0 }}
+      transition={{ duration: 1 }}
+    >
+      {children}
+    </motion.section>
+  );
+};
+
+export default WrapperRevealer;
