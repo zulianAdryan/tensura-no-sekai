7f65f39cd766fcfe7bc65e0899a18141b7e357c7
zulianAdryan <zulian.adryan@gmail.com>
2024-07-23 14:06:07 +0700

    feat: constant video references

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,24 @@
+import { SourceHTMLAttributes, VideoHTMLAttributes } from "react";
+
 export const API = {
   BASE_URL: "https://api.jikan.moe/v4",
   ANIME_ID: "37430",
+};
+
+export const VIDEO_SOURCE: {
+  [key: string]: Pick<
+    VideoHTMLAttributes<HTMLVideoElement>,
+    "src" | "poster"
+  > & { type: SourceHTMLAttributes<HTMLSourceElement>["type"] };
+} = {
+  INTRO: {
+    src: "/intro.mp4",
+    poster: "",
+    type: "video/mp4",
+  },
+  HOME: {
+    src: "/home.mp4",
+    poster: "/poster-home.jpg",
+    type: "video/mp4",
+  },
 };
